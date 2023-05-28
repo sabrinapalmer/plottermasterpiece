851dbac149839822d8e8934b8cdcfcdcb337d411
sabrinapalmer <sabrinampalmer@gmail.com>
2023-05-27 21:03:31 -0500

    added export button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import Canvas from './Canvas';
 import Sidebar from './Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  exportButton: {
+    position: 'absolute',
+    bottom: '10px',
+    right: '10px',
+    zIndex: '10',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   const [circleCount, setCircleCount] = useState(1000);
   const [colorCount, setColorCount] = useState(2);
   const [colors, setColors] = useState(['#8db4dd', '#FA91FA']);
@@ -11,6 +23,15 @@ function App() {
 
   const handleRegenerateCanvas = () => {
     setCanvasKey((prevKey) => prevKey + 1);
+  };
+
+  const handleExportCanvas = () => {
+    const canvas = document.querySelector('canvas');
+    const image = canvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'sketch.png';
+    link.click();
   };
 
   return (
@@ -34,6 +55,14 @@ function App() {
           colors={colors}
           sizeRange={sizeRange}
         />
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.exportButton}
+          onClick={handleExportCanvas}
+        >
+          Export
+        </Button>
       </div>
     </div>
   );
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,97 +1,78 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import p5 from 'p5';
 
 const useStyles = makeStyles((theme) => ({
   canvasContainer: {
-    width: '800px', // Set the desired width of the canvas container
-    height: '600px', // Set the desired height of the canvas container
-    margin: '0 auto', // Center the container horizontally
-    marginTop: '50vh', // Center vertically using 50% viewport height
-    transform: 'translateY(-50%)', // Adjust vertical position to center
-    border: '2px solid black', // Add a border
-    borderRadius: theme.spacing(1), // Add border radius
-    overflow: 'hidden', // Hide any overflowing content
-    display: 'flex', // Add flex display
-    justifyContent: 'center', // Center horizontally
-    alignItems: 'center', // Center vertically
+    width: '800px',
+    height: '600px',
+    margin: '0 auto',
+    marginTop: '50vh',
+    transform: 'translateY(-50%)',
+    border: '2px solid black',
+    borderRadius: theme.spacing(1),
+    overflow: 'hidden',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 }));
 
-const Canvas = ({ circleCount, colorRange, sizeRange, colors, regenerateFlag }) => {
+const Canvas = ({ circleCount, colorCount, colors, sizeRange }) => {
   const classes = useStyles();
   const canvasRef = useRef(null);
-  const circlesRef = useRef([]);
-  const [shouldRegenerate, setShouldRegenerate] = useState(false);
+  const [p, setP] = useState(null);
 
-  const regenerateCircles = () => {
-    circlesRef.current = [];
+  const regenerateCircles = (p) => {
+    p.clear();
+    p.background(255);
 
     for (let i = 0; i < circleCount; i++) {
-      const size = getRandomValue(sizeRange[0], sizeRange[1]);
-      const randomColorIndex = Math.floor(Math.random() * colors.length);
+      const size = p.random(sizeRange[0], sizeRange[1]);
+      const randomColorIndex = Math.floor(p.random(colors.length));
       const newColor = colors[randomColorIndex];
-      const x = getRandomValue(0, canvasRef.current.width);
-      const y = getRandomValue(0, canvasRef.current.height);
-      const circle = { x, y, size, color: newColor };
-      circlesRef.current.push(circle);
-    }
-
-    drawCircles();
-  };
-
-  const drawCircles = () => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < circlesRef.current.length; i++) {
-      const { x, y, size, color } = circlesRef.current[i];
-      context.beginPath();
-      context.arc(x, y, size, 0, 2 * Math.PI);
-      context.fillStyle = color;
-      context.fill();
-      context.closePath();
+      const x = p.random(p.width);
+      const y = p.random(p.height);
+      p.fill(newColor);
+      p.noStroke();
+      p.circle(x, y, size);
     }
   };
 
-  const getRandomValue = (min, max) => {
-    return Math.random() * (max - min) + min;
+  const handleRegenerateCanvas = () => {
+    if (p) {
+      regenerateCircles(p);
+    }
   };
 
   useEffect(() => {
-    const handleWindowResize = () => {
-      canvasRef.current.width = window.innerWidth;
-      canvasRef.current.height = window.innerHeight;
-      regenerateCircles();
+    const sketch = (p) => {
+      p.setup = () => {
+        const pixelDensity = window.devicePixelRatio*4 || 1; // Get the pixel density of the device
+        const canvas = p.createCanvas(window.innerWidth, window.innerHeight).parent(canvasRef.current);
+        canvas.style.width = '100%'; // Make the canvas responsive
+        canvas.style.height = '100%';
+        p.pixelDensity(pixelDensity);
+        regenerateCircles(p);
+      };
+
+      p.windowResized = () => {
+        p.resizeCanvas(window.innerWidth, window.innerHeight);
+        regenerateCircles(p);
+      };
     };
 
-    window.addEventListener('resize', handleWindowResize);
-
-    canvasRef.current.width = window.innerWidth;
-    canvasRef.current.height = window.innerHeight;
-    regenerateCircles();
+    const newP = new p5(sketch);
+    setP(newP);
 
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      newP.remove();
     };
   }, []);
 
-  useEffect(() => {
-    if (shouldRegenerate) {
-      regenerateCircles();
-      setShouldRegenerate(false);
-    }
-  }, [shouldRegenerate]);
-
-  const handleRegenerateCanvas = () => {
-    setShouldRegenerate(true);
-  };
-
   return (
     <div className={classes.canvasContainer}>
-      <canvas ref={canvasRef} />
-      <button onClick={handleRegenerateCanvas} style={{ display: 'none' }}></button>
+      <div ref={canvasRef}></div>
     </div>
   );
 };
